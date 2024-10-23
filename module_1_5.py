e26f47c844ee13136523ad651e38d18e7dd0319b
pepefrog228228 <pepefrog228228228@gmail.com>
2024-10-23 23:38:17 +0500

    Delete module_1_5.py

--- module_1_5.py
+++
@@ -1,7 +0,0 @@
-immutable_var = 8, "red", 11
-print(immutable_var)
-immutable_var[0] = 228  #кортеж не поддерживает обращение по элементам
-print(immutable_var)
-mutable_list = [3, 9, 6 , "Jojo"]
-mutable_list.append("Bizarre Adventure")
-print(mutable_list)
