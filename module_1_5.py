2fc786f9751faef17b92aa32e4510a351879f47e
pepefrog228228 <pepefrog228228228@gmail.com>
2024-10-23 23:39:45 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 8, "red", 11
+print(immutable_var)
+#immutable_var[0] = 228  (кортеж не поддерживает обращение по элементам)
+mutable_list = [3, 9, 6 , "Jojo"]
+mutable_list.append("Bizarre Adventure")
+print(mutable_list)
